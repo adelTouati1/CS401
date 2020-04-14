f304e46506e7fabd739c058bd300c7eb4d6b0c44
adelTouati1 <adeltouati@u.boisestate.edu>
2020-04-14 16:06:06 -0400

    Add files via upload

--- photographyzone/handlers_scripts/classes/Dao.php
+++ photographyzone/handlers_scripts/classes/Dao.php
@@ -4,7 +4,7 @@ class Dao
 {
     private $host = 'localhost';
     private $dbname = 'photographyZone';
-    private $user = 'root@localhost';
+    private $user = 'root';
     private $password = 'Adel1234!';
 
     public function addUser($firstName, $lastName, $email, $password)
@@ -43,9 +43,11 @@ class Dao
     {
         try {
 			return
-            $conn = new PDO("mysql:host={$this->host}:3306;dbname={$this->dbname}", $this->user,
-                    $this->password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);     
+            //$conn = new PDO("mysql:host={$this->host}:3306;dbname={$this->dbname}", $this->user,
+                    //$this->password);
+            //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+            
+            $conn = new mysqli($host, $user, $password,$dbname);
 		} catch (Exception $e) {
 			echo "connection failed: " . $e->getMessage();
 		}
