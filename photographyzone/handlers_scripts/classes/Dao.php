28accd3020ba97227fca2daf832f1f04a00d42ce
adelTouati1 <adeltouati@u.boisestate.edu>
2020-04-14 16:21:01 -0400

    Add files via upload

--- photographyzone/handlers_scripts/classes/Dao.php
+++ photographyzone/handlers_scripts/classes/Dao.php
@@ -2,7 +2,7 @@
 
 class Dao
 {
-    private $host = 'localhost';
+    private $host = '127.0.0.1';
     private $dbname = 'photographyZone';
     private $user = 'root';
     private $password = 'Adel1234!';
