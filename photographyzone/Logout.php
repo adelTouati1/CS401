799001a45b3d3cfb3d4327a440718ad1d677bda3
adelTouati1 <adeltouati@u.boisestate.edu>
2020-04-16 15:09:43 -0400

    new

--- photographyzone/Logout.php
+++ photographyzone/Logout.php
@@ -3,5 +3,6 @@ session_start();
 if (isset($_SESSION['AUTH'])) {
     $_SESSION['AUTH'] = FALSE;
 }
-
+session_unset();
+session_destroy();
 header("Location:index.php");
--- photographyzone/index.php
+++ photographyzone/index.php
@@ -14,8 +14,8 @@ include_once('index.php');
    <div>
      <form>
      <?php if ($_SESSION['AUTH']): ?>
-      
-    <MARQUEE bgcolor="#CCCCCC" loop="-1" scrollamount="2" width="100%"><?php echo $_SESSION['name'] ?></MARQUEE>
+
+    <MARQUEE bgcolor=white loop="-1" scrollamount="2" width="100%"><?php echo $_SESSION['name'] ?></MARQUEE>
     <input class="MyButton" type="button" value="Log out" onclick="href='Logout.php'"/>
     <?php else: ?>
      <input class="MyButton" type="button" value="Login" onclick="window.location.href='login.html'" />
