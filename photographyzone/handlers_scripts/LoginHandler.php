78e96cc3f38fa7c0894b80ae73c3bcd730268f6d
adelTouati1 <adeltouati@u.boisestate.edu>
2020-04-20 15:45:24 -0400

    new

--- photographyzone/handlers_scripts/LoginHandler.php
+++ photographyzone/handlers_scripts/LoginHandler.php
@@ -8,16 +8,17 @@ $dao = new Dao();
 $email = htmlentities($_POST['email']);
 $password = $_POST['password'];
 if (! $dao->checkEmailExists($email)) {
-	$_SESSION["error"] = "Email or Password invalid";
+    $_SESSION["error"] = "Email or Password invalid";
+    header("Location:../login.php");
   } elseif ($dao->validateUser($email, $password) === false) {
     $_SESSION["error"] = "Email or Password invalid";
     $_SESSION['AUTH'] = FALSE;
+    header("Location:../login.php");
   }  else {
 	unset($_SESSION["error"]);
     $_SESSION['AUTH'] = true;
     $_SESSION['name'] = $dao->getFirstName($email)['firstname'];
     header("Location:../index.php");
   }
-  header("Location:../login.php");
 }
   exit;
