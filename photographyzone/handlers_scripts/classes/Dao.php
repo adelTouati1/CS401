ead8549bfac2d1db7d8ee8c9bb7e3f7cc32ddffb
adelTouati1 <adeltouati@u.boisestate.edu>
2020-04-21 14:32:04 -0400

    new

--- photographyzone/handlers_scripts/classes/Dao.php
+++ photographyzone/handlers_scripts/classes/Dao.php
@@ -44,7 +44,7 @@ class Dao
        
         $conn = $this->getConnection();
 		$query = $conn->prepare("INSERT INTO images (email, location, camerabrand, lensesize, focus, description, picture) 
-        VALUES (:email, :location, :camerabrand, :lensesize, :focus, :description, :picture)");
+        VALUES (:email, :location, :camerabrand, :lensesize, :focus, :description, ?)");
         $query->bindParam(':email', $email);
         $query->bindParam(':location', $location);
 		$query->bindParam(':camerabrand', $camerabrand);
