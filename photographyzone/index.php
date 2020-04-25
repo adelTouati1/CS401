361d1a0b8cf371617310508acf775db1c5912dd1
adelTouati1 <adeltouati@u.boisestate.edu>
2020-04-25 15:52:32 -0400

    new

--- photographyzone/index.php
+++ photographyzone/index.php
@@ -13,6 +13,7 @@ include_once('index.php');
 
     <head>
         <title>Photography Zone</title>
+        <link rel="icon" href="favicon.ico">
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
 
